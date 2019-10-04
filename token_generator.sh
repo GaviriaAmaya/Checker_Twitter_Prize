ec6a533edebb022b2675b9f370f190d147e0fbdb
GaviriaAmaya <judamayaga@unal.edu.co>
2019-10-04 19:28:49 +0000

    [Update] Authentication data petition is just done once. If auth.json exists, the token generator creates a new API key

--- token_generator.sh
+++ token_generator.sh
@@ -5,36 +5,37 @@ if [ ! -f auth_data.json ]
 
 then
     echo "{\"api_key\": \"YOUR_API_KEY\", \"email\": \"YOUR_EMAIL\", \"password\": \"YOUR_PASS\", \"scope\": \"checker\"}" > auth_data.json
+
+    echo -n "  -> Holberton Intranet API Key: "
+    read -r api_key
+    echo -n "  -> Holberton Intranet email: "
+    read -r email
+    echo -n "  -> Holberton Intranet password: "
+    read -r password
+
+    # & escaper
+    password=$(sed 's/[\*\.&]/\\&/g' <<<"$password")
+
+    if grep -q YOUR_API_KEY auth_data.json
+    then
+	sed -i "s/YOUR_API_KEY/$api_key/g" auth_data.json
+    fi
+
+    if grep -q YOUR_EMAIL auth_data.json
+    then
+	sed -i "s/YOUR_EMAIL/$email/g" auth_data.json
+    fi
+
+    if grep -q YOUR_PASS auth_data.json
+    then
+	sed -i "s/YOUR_PASS/$password/g" auth_data.json
+    fi
+
 else
     echo "JSON Exists: Proceding..."
 fi
 
-echo -n "  -> Holberton Intranet API Key: "
-read -r api_key
-echo -n "  -> Holberton Intranet email: "
-read -r email
-echo -n "  -> Holberton Intranet password: "
-read -r password
-
-# & escaper
-password=$(sed 's/[\*\.&]/\\&/g' <<<"$password")
-
-if grep -q YOUR_API_KEY auth_data.json
-then
-    sed -i "s/YOUR_API_KEY/$api_key/g" auth_data.json
-fi
-
-if grep -q YOUR_EMAIL auth_data.json
-then
-    sed -i "s/YOUR_EMAIL/$email/g" auth_data.json
-fi
-
-if grep -q YOUR_PASS auth_data.json
-then
-    sed -i "s/YOUR_PASS/$password/g" auth_data.json
-fi
-
-curl -XPOST https://intranet.hbtn.io/users/auth_token.json -H "Content-Type: application/json" -d @auth_data.json >> auth_token.json
+rm auth_token.json && curl -XPOST https://intranet.hbtn.io/users/auth_token.json -H "Content-Type: application/json" -d @auth_data.json >> auth_token.json
 
 
 echo "All set!"
